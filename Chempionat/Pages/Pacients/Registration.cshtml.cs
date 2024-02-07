57d74787e5154775c21f743433afb0bbb09f104d
Student <Student@CAB4-4>
2024-02-07 14:37:19 +1000

    -

--- Chempionat/Pages/Pacients/Registration.cshtml.cs
+++ Chempionat/Pages/Pacients/Registration.cshtml.cs
@@ -8,20 +8,40 @@ namespace Chempionat.Pages.Pacients
 {
     public class RegistrationModel : PageModel
     {
-        static List<Gender> genders { get; } = new List<Gender>
-        {
-            new Gender{GendersName="Мужской"},
-            new Gender{GendersName="Женский"},
-            new Gender{GendersName="Боевой вертолётик"},
-        };
-        public SelectList Genders { get; } = new SelectList(genders, "Id", "GendersName");
-		
-        public SelectList Jobs { get; } = new SelectList(DataBase.instanse.InsurancePolicies.ToList(), "Id", 
+        public SelectList Genders { get; } = new SelectList(DataBase.instanse.Genders, "Id", "GendersName");
 
+        public List<InsurancyCompany> InsurancyCompaniesFirst { get; } = DataBase.instanse.InsurancyCompanies.ToList();
+        public SelectList InsurancyCompanies { get; } = new SelectList(DataBase.instanse.InsurancyCompanies.ToList(), "Id", "InsurancyCompaniesTitle");
+
+        [BindProperty] 
 		public int GenderId { get; set; }
+		[BindProperty]
+		public int InsuranceCompanyId { get; set; }
 
-        public void OnGet()
+        [BindProperty]  
+        public Pacient NewPacient { get; set; }
+
+        [BindProperty]
+        public InsurancePolicy NewInsurancePolicy { get; set; }
+
+        public record class Vaslues(string jobName, string PasportSeries, string PasportNumber);
+
+		public void OnGet()
         {
+
         }
+        public void OnPost(Vaslues values)
+        {
+            Job job = DataBase.instanse.Jobs.FirstOrDefault(s => s.JobsCompanyName == values.jobName);
+
+            if (job == null)
+            {
+				DataBase.instanse.Jobs.Add(job);
+                DataBase.instanse.SaveChanges();
+			}
+
+            NewPacient.PacientsJobId = DataBase.instanse.Jobs.First(s => s.JobsCompanyName == job.JobsCompanyName).Id;
+
+		}
     }
 }
